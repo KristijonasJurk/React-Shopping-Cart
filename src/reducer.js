4ac29bdd73349b8bd3b329c2bf7eac75da6902ba
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-22 17:51:21 +0200

    increase button

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,21 @@
 const reducer = (state, action) => {
-    if (action.type == 'CLEAR_CART') {
+    if (action.type === 'CLEAR_CART') {
         return { ...state, cart: [] }
     }
-    if (action.type == 'REMOVE') {
+    if (action.type === 'REMOVE') {
         return {
             ...state,
             cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
         }
+    }
+    if (action.type === 'INCREASE') {
+        const tempCart = state.cart.map((cartItem) => {
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount + 1 }
+            }
+            return cartItem;
+        })
+        return { ...state, cart: tempCart }
     }
     return state
 }
